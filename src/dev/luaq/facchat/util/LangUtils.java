78d84510c46743b33e92de566fc8b9c4753f7a24
luaqs <joshbirchlol@gmail.com>
2021-03-30 10:37:18 -0400

    rename: ChatUtils -> LangUtils with config.yml assistance

--- src/dev/luaq/facchat/util/LangUtils.java
+++ src/dev/luaq/facchat/util/LangUtils.java
@@ -1,8 +1,23 @@
 package dev.luaq.facchat.util;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.bukkit.ChatColor;
+import org.bukkit.configuration.ConfigurationSection;
 
-public class ChatUtils {
+public class LangUtils {
+    @Getter @Setter
+    private static ConfigurationSection langSect;
+
+    public static String langf(String path, Object... format) {
+        if (!langSect.contains(path)) {
+            return String.format("'lang.%s' was not found", path);
+        }
+
+        String lang = langSect.getString(path);
+        return colorf(lang, format);
+    }
+
     public static String colorf(String input, Object... format) {
         return color(String.format(input, format));
     }
