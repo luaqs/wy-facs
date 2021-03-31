0f432c840e363441717b0d990b83dea46f748464
luaqs <joshbirchlol@gmail.com>
2021-03-31 11:45:08 -0400

    add: broken admin command base

--- src/dev/luaq/facchat/command/FacAdmin.java
+++ src/dev/luaq/facchat/command/FacAdmin.java
@@ -2,7 +2,10 @@ package dev.luaq.facchat.command;
 
 import dev.luaq.facchat.factions.Faction;
 import dev.luaq.facchat.factions.FactionManager;
+import dev.luaq.facchat.factions.player.FactionPlayer;
+import dev.luaq.facchat.util.CommandUtils;
 import dev.luaq.facchat.util.FactionUtils;
+import dev.luaq.facchat.util.LangUtils;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -15,8 +18,99 @@ import java.util.List;
 public class FacAdmin implements CommandExecutor, TabCompleter {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
-        // TODO: 2021-03-31 god fucking dammit, look at onTabComplete, this is gonna suck LOL
+        // no need to check if sender is a Player, console can use these
+
+        // no arguments, send the usage
+        if (args.length == 0) {
+            CommandUtils.sendUsage(sender, command);
+            return true;
+        }
+
+        // get the faction here, because why not
+        Faction faction = null;
+        if (args.length >= 1) {
+            faction = FactionManager.getManager().getFaction(args[1]);
+        }
+
+        // go through the sub commands
+        String sub = args[0].toLowerCase();
+        switch (sub) {
+            case "add":
+            case "remove":
+                // woah, a hardcoded message
+                sender.sendMessage("Let's not do this, use the config if you want to change existing factions.");
+                break;
+
+            case "mod":
+                handleMod(sender, command, args, faction);
+                break;
+
+            default: // invalid subcommand
+                CommandUtils.sendUsage(sender, command);
+                break;
+        }
+
         return true;
+    }
+
+    private void handleMod(CommandSender sender, Command command, String[] args, Faction faction) {
+        if (faction == null) {
+            sender.sendMessage(LangUtils.langf("faction.error.noexisting", args.length >= 1 ? args[1] : ""));
+            return;
+        }
+
+        String sub, value;
+        if (args.length < 4) {
+            CommandUtils.sendUsage(sender, command);
+            return;
+        }
+
+        // the subcommand and the value of it
+        sub = args[2].toLowerCase();
+        value = args[3];
+
+        FactionManager manager = FactionManager.getManager();
+        // parse the value as a player if possible
+        FactionPlayer player = null;
+
+        switch (sub) {
+            case "leader":
+                // the new leader of the faction
+                faction.setLeader(player.getUuid());
+                break;
+
+            case "max":
+                int newMax;
+                try {
+                    newMax = Integer.parseInt(value);
+                } catch (NumberFormatException e) {  break; }
+
+                faction.setMaxMembers(newMax);
+                break;
+
+            case "name":
+                faction.setName(value);
+                break;
+
+            case "addmember":
+                if (player == null) {
+                    break;
+                }
+
+                // set the player's faction
+                player.setFactionName(faction.getAbbr());
+                break;
+
+            case "removemember":
+                if (player == null) {
+                    break;
+                }
+
+                // remove their faction name
+                manager.clearPlayerFaction(player.getUuid());
+
+                break;
+        }
     }
 
     @Override
